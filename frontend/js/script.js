84f266ec52ac3fa989980c67fb8fa4cd0e128bc8
Danai Akrivou <wi23b026@technikum-wien.at>
2025-04-28 20:50:33 +0200

    Design change

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -7,3 +7,40 @@ fetch(`http://${window.location.hostname}:3000/api/test`)
     .catch(err => {
         console.error('Fehler beim Laden:', err);
     });
+
+fetch('includes/nav-bar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    });
+
+function loadContent(page) {
+    let file = '';
+
+    switch (page) {
+        case 'home':
+            file = 'sites/home.html';
+            break;
+        case 'products':
+            file = 'sites/products.html';
+            break;
+        case 'register':
+            file = 'sites/register.html';
+            break;
+        case 'login':
+            file = 'sites/login.html';
+            break;
+        default:
+            file = 'sites/home.html';
+    }
+
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+}
+
+window.onload = () => {
+    loadContent('home');
+};
